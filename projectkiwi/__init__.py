e9cd5d09c4760e60f555250210e8f929da02c6db
Michael Thoreau <michael@thoreau.com.au>
2024-09-04 13:35:07 -0700

    initial working commit

---
+++ projectkiwi/__init__.py
@@ -0,0 +1,29 @@
+"""
+projectkiwi
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module is a means for interfacing with projectkiwi using python.
+
+
+Classes:
+    - ProjectkiwiClient: A class to interface with project kiwi
+
+Example:
+    To get started, try this:
+
+    >>> from projectkiwi import ProjectkiwiClient
+    >>> client = ProjectKiwiClient("my_key)
+
+
+Author:
+    Your Name <michael@projectkiwi.io>
+    
+License:
+    MIT License (if applicable)
+"""
+
+
+# your_package/__init__.py
+from .Client import Client
+
+__version__ = "0.1.0"
